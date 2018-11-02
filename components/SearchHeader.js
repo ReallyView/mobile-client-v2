7bfabb3215197e760ccb19796ec74494053abb9d
byeongjo <gee05053@kookmin.ac.kr>
2018-11-03 00:41:00 +0900

    SearchHeader_android.js를 삭제 했습니다, SearchHeader.js에 삭제한 파일 내용을 합쳤습니다.

--- components/SearchHeader.js
+++ components/SearchHeader.js
@@ -5,18 +5,34 @@ import {
   Icon,
   Input,
   Item,
-  Text
+  Text, Body, Right
 } from 'native-base'
 import { Platform } from 'react-native'
 import Layout from '../constants/Layout'
-import SearchHeaderForAndroid from './SearchHeader_android'
 
 const platfrom = Platform.OS
 
 export default class SearchHeader extends React.Component {
   render () {
     if (platfrom === 'android') {
-      return <SearchHeaderForAndroid />
+      return (
+        <Header>
+          <Button transparent style={{ marginLeft: -0.04 * Layout.window.width }} onPress={this.props.onClickBackButton}>
+            <Text>Back</Text>
+          </Button>
+          <Body>
+            <Item>
+              <Icon name='search' />
+              <Input placeholder='Search' />
+            </Item>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Text>Search</Text>
+            </Button>
+          </Right>
+        </Header>
+      )
     }
     return (
       <Header searchBar rounded>
