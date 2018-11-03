ead9947bb6ce023d883b4c163cc5248ddd78b91f
byeongjo <gee05053@kookmin.ac.kr>
2018-11-04 01:21:17 +0900

    프로필 이미지 크기 수정

--- components/MoreContent.js
+++ components/MoreContent.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Content, Card, CardItem, Left, Thumbnail, Body, Text } from 'native-base'
+import { Content, Card, CardItem, Left, Thumbnail, Body, Text, List, ListItem } from 'native-base'
+import Layout from '../constants/Layout'
 
 export default class MoreContent extends React.Component {
   render () {
@@ -8,26 +9,33 @@ export default class MoreContent extends React.Component {
         <Card transparent>
           <CardItem>
             <Left>
-              <Thumbnail square large source={{ uri: this.props.profileImgUrl }} />
+              <Thumbnail square source={{ uri: this.props.profileImgUrl }} style={{ width: 90, height: 90 }} />
               <Body>
-                <Text>email: {this.props.email}</Text>
-                <Text>name: {this.props.name}</Text>
+                <Text style={{ fontSize: 0.045 * Layout.window.width }}>email: {this.props.email}</Text>
+                <Text />
+                <Text />
+                <Text />
+                <Text />
+                <Text />
+                <Text style={{ fontSize: 0.045 * Layout.window.width }}>name: {this.props.name}</Text>
               </Body>
             </Left>
           </CardItem>
-          <CardItem>
-            <Text>프로필 수정</Text>
-          </CardItem>
-          <CardItem>
-            <Text>공지사항</Text>
-          </CardItem>
-          <CardItem>
-            <Text>버전</Text>
-          </CardItem>
-          <CardItem>
-            <Text>라이센스</Text>
-          </CardItem>
         </Card>
+        <List>
+          <ListItem>
+            <Text>프로필 수정</Text>
+          </ListItem>
+          <ListItem>
+            <Text>공지사항</Text>
+          </ListItem>
+          <ListItem>
+            <Text>버전</Text>
+          </ListItem>
+          <ListItem>
+            <Text>라이센스</Text>
+          </ListItem>
+        </List>
       </Content>
     )
   }
