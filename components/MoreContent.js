f551f45b8a1ce67205dbf171925514f093466a24
byeongjo <gee05053@kookmin.ac.kr>
2018-11-07 15:09:06 +0900

    MoreView의 content에서 profileImgUrl, email, name을 AsyncStorage에서 가져오도록 했습니다.

--- components/MoreContent.js
+++ components/MoreContent.js
@@ -1,23 +1,40 @@
 import React from 'react'
 import { Content, Card, CardItem, Left, Thumbnail, Body, Text, List, ListItem } from 'native-base'
+import { AsyncStorage } from 'react-native'
 import Layout from '../constants/Layout'
 
 export default class MoreContent extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      profileImgUrl: undefined,
+      email: '',
+      name: ''
+    }
+  }
+  componentWillMount () {
+    const getData = async () => {
+      const profile = await AsyncStorage.getItem('profileImgUrl')
+      const email = await AsyncStorage.getItem('email')
+      const name = await AsyncStorage.getItem('name')
+      this.setState({
+        profileImgUrl: profile,
+        email: email,
+        name: name
+      })
+    }
+    getData()
+  }
   render () {
     return (
       <Content>
         <Card transparent>
           <CardItem>
             <Left>
-              <Thumbnail square source={{ uri: this.props.profileImgUrl }} style={{ width: 90, height: 90 }} />
+              <Thumbnail square source={{ uri: this.state.profileImgUrl }} style={{ width: 90, height: 90 }} />
               <Body>
-                <Text style={{ fontSize: 0.045 * Layout.window.width }}>email: {this.props.email}</Text>
-                <Text />
-                <Text />
-                <Text />
-                <Text />
-                <Text />
-                <Text style={{ fontSize: 0.045 * Layout.window.width }}>name: {this.props.name}</Text>
+                <Text style={{ fontSize: 0.045 * Layout.window.width }}>email: {this.state.email}</Text>
+                <Text style={{ fontSize: 0.045 * Layout.window.width, marginTop: 20 }}>name: {this.state.name}</Text>
               </Body>
             </Left>
           </CardItem>
