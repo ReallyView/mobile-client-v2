b3d26170e91350aab2d42c017899444aca499e3e
byeongjo <gee05053@kookmin.ac.kr>
2018-11-09 23:19:38 +0900

    AddReviewView에 있던 state와 function들을 AddReviewContent이동, AddReviewView에서 Search기능을 할 수 있도록 구현 했습니다.

--- Views/AddReviewView.js
+++ Views/AddReviewView.js
@@ -4,81 +4,11 @@ import Headers from '../components/Headers'
 import AddReviewContent from '../components/AddReviewContent'
 
 export default class AddReviewView extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      addTitle: '',
-      addItemName: '',
-      categoryId: undefined,
-      text: '',
-      grades: [{ name: '', starNum: 1 }],
-      itemId: ''
-    }
-    this.onChangeTitle = this.onChangeTitle.bind(this)
-    this.onChangeItemName = this.onChangeItemName.bind(this)
-    this.onChangeCategoryId = this.onChangeCategoryId.bind(this)
-    this.onIncreaseStarNum = this.onIncreaseStarNum.bind(this)
-    this.onChangeGradeName = this.onChangeGradeName.bind(this)
-    this.onClickAddGrade = this.onClickAddGrade.bind(this)
-    this.onChangeReviewText = this.onChangeReviewText.bind(this)
-  }
-  onChangeTitle (title) {
-    this.setState({
-      addTitle: title
-    })
-  }
-  onChangeItemName (itemName) {
-    this.setState({
-      addItemName: itemName
-    })
-  }
-  onChangeCategoryId (value) {
-    this.setState({
-      categoryId: value
-    })
-  }
-  onClickAddGrade () {
-    let temp = [{ name: '', starNum: 1 }]
-    this.setState({
-      grades: this.state.grades.concat(temp)
-    })
-  }
-  onChangeGradeName (name, index) {
-    let temp = this.state.grades
-    temp[index].name = name
-    this.setState({
-      grades: temp
-    })
-  }
-  onIncreaseStarNum (starNum, index) {
-    let tempStarNum = (starNum < 5) ? starNum + 1 : 1
-    let temp = this.state.grades
-    temp[index].starNum = tempStarNum
-    this.setState({
-      grades: temp
-    })
-  }
-  onChangeReviewText (Text) {
-    this.setState({
-      text: Text
-    })
-  }
-  // 이미지 넣는 함수
   render () {
     return (
       <Container>
         <Headers navigation={this.props.navigation} headerTitle={'리뷰추가'} />
-        <AddReviewContent
-          categoryId={this.state.categoryId}
-          grades={this.state.grades}
-          onChangeTitle={this.onChangeTitle}
-          onChangeItemName={this.onChangeItemName}
-          onChangeCategoryId={this.onChangeCategoryId}
-          onIncreaseStarNum={this.onIncreaseStarNum}
-          onChangeGradeName={this.onChangeGradeName}
-          onClickAddGrade={this.onClickAddGrade}
-          onChangeReviewText={this.onChangeReviewText}
-        />
+        <AddReviewContent />
       </Container>
     )
   }
--- components/AddReviewContent.js
+++ components/AddReviewContent.js
@@ -1,32 +1,116 @@
 import React from 'react'
-import { Card, CardItem, Content, Textarea, Text, Left, Button, Body } from 'native-base'
+import { Card, CardItem, Content, Textarea, Text, Left, Button, Body, Form } from 'native-base'
 import AddReviewItemName from '../components/AddReviewItemName'
 import AddReviewCategory from '../components/AddReviewCategory'
 import AddReviewTitleName from '../components/AddReviewTitleName'
 import AddReviewGradeGroup from '../components/AddReviewGradeGroup'
 import AddReviewButtons from '../components/AddReviewButtons'
+import { SearchProduct } from './AddReviewSearchProduct'
 
 export default class RequestAndCompareContent extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      addTitle: '',
+      addItemName: '',
+      addItemId: '',
+      categoryId: 'null',
+      text: '',
+      grades: [{ name: '', starNum: 1 }],
+      itemId: '',
+      isItemReady: false
+    }
+    this.onChangeTitle = this.onChangeTitle.bind(this)
+    this.onChangeItemName = this.onChangeItemName.bind(this)
+    this.onChangeCategoryId = this.onChangeCategoryId.bind(this)
+    this.onIncreaseStarNum = this.onIncreaseStarNum.bind(this)
+    this.onChangeGradeName = this.onChangeGradeName.bind(this)
+    this.onClickAddGrade = this.onClickAddGrade.bind(this)
+    this.onChangeReviewText = this.onChangeReviewText.bind(this)
+    this.onChangeItemId = this.onChangeItemId.bind(this)
+    this.onChangeItemNameAndId = this.onChangeItemNameAndId.bind(this)
+  }
+  onChangeTitle (title) {
+    this.setState({
+      addTitle: title
+    })
+  }
+  onChangeItemName (itemName) {
+    this.setState({
+      addItemName: itemName,
+      isItemReady: true
+    })
+  }
+  onChangeCategoryId (value) {
+    this.setState({
+      categoryId: value
+    })
+  }
+  onClickAddGrade () {
+    let temp = [{ name: '', starNum: 1 }]
+    this.setState({
+      grades: this.state.grades.concat(temp)
+    })
+  }
+  onChangeGradeName (name, index) {
+    let temp = this.state.grades
+    temp[index].name = name
+    this.setState({
+      grades: temp
+    })
+  }
+  onIncreaseStarNum (starNum, index) {
+    let tempStarNum = (starNum < 5) ? starNum + 1 : 1
+    let temp = this.state.grades
+    temp[index].starNum = tempStarNum
+    this.setState({
+      grades: temp
+    })
+  }
+  onChangeReviewText (Text) {
+    this.setState({
+      text: Text
+    })
+  }
+  onChangeItemId (id) {
+    this.setState({
+      addItemId: id
+    })
+  }
+  onChangeItemNameAndId (name, id) {
+    this.onChangeItemName(name)
+    this.onChangeItemId(id)
+    this.setState({
+      isItemReady: false
+    })
+  }
+  // 이미지 넣는 함수
   render () {
     return (
       <Content >
         <Card transparent>
           <CardItem>
-            <AddReviewTitleName onChangeTitle={this.props.onChangeTitle} />
+            <AddReviewTitleName onChangeTitle={this.onChangeTitle} />
           </CardItem>
         </Card>
         <Card transparent>
           <CardItem>
-            <AddReviewItemName onChangeItemName={this.props.onChangeItemName} />
-            <AddReviewCategory categoryId={this.props.categoryId} onChangeCategoryId={this.props.onChangeCategoryId} />
+            <AddReviewItemName addItemName={this.state.addItemName} onChangeItemName={this.onChangeItemName} />
+            <AddReviewCategory categoryId={this.state.categoryId} onChangeCategoryId={this.onChangeCategoryId} />
           </CardItem>
         </Card>
+        <CardItem>
+          {
+            (this.state.isItemReady && this.state.addItemName)
+              ? <SearchProduct itemName={this.state.addItemName} onChangeItemNameAndId={this.onChangeItemNameAndId} /> : <Form />
+          }
+        </CardItem>
         <Card transparent style={{ marginTop: 25 }}>
           <Textarea
             rowSpan={5}
             bordered
             placeholder='리뷰를 입력하세요'
-            onChangeText={(Text) => this.props.onChangeReviewText(Text)}
+            onChangeText={(Text) => this.onChangeReviewText(Text)}
           />
         </Card>
         <Card transparent>
@@ -35,16 +119,16 @@ export default class RequestAndCompareContent extends React.Component {
               <Text>평점</Text>
             </Left>
             <Body>
-              <Button onPress={this.props.onClickAddGrade}>
+              <Button onPress={this.onClickAddGrade}>
                 <Text>평점 추가하기</Text>
               </Button>
             </Body>
           </CardItem>
           <CardItem>
             <AddReviewGradeGroup
-              grades={this.props.grades}
-              onIncreaseStarNum={this.props.onIncreaseStarNum}
-              onChangeGradeName={this.props.onChangeGradeName}
+              grades={this.state.grades}
+              onIncreaseStarNum={this.onIncreaseStarNum}
+              onChangeGradeName={this.onChangeGradeName}
             />
           </CardItem>
         </Card>
